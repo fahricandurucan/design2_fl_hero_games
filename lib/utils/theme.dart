eb35dd7b64b2d1982068fc82249c224f97ef7727
fahricandurucan <fahricandurucan@icloud.com>
2023-09-23 22:30:07 +0300

    small changes

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -6,22 +6,14 @@ class CColors {
   static const textColor = Color(0xffCECECE);
   static const backgroundcolor = Color(0xff161616);
   static const iconColor = Color.fromARGB(255, 112, 112, 112);
-  static const foregroundBlack = Color(0xff202020);
   static const subtitleColor = Color(0xff979797);
   static const sideColor = Color(0x4cb7b7b3);
   static const black = Colors.black;
-  static const green = Colors.green;
-  static const orange = Colors.orange;
-  static const red = Colors.red;
-  static const tagColor = Color(0xffD9D9D9);
-  static const logoRed = Color(0xffe75236);
-  static const logoOrange = Color(0xffe68c35);
-  static const logoYellow1 = Color(0xffdbae34);
-  static const logoYellow2 = Color(0xffbece34);
   static const darkBlue = Color(0xFF061F33);
   static const backgroundColor = Color.fromARGB(255, 242, 242, 255);
   static const dividerColor = Color.fromARGB(255, 172, 172, 175);
   static const healthIconColor = Color.fromARGB(255, 24, 67, 102);
+
   static const Map<int, Color> primarySwatchColors = {
     50: Color.fromRGBO(0, 120, 212, .1),
     100: Color.fromRGBO(0, 120, 212, .2),
@@ -39,11 +31,6 @@ class CColors {
 }
 
 class Styles {
-  static TextStyle get evenBiggerTitle => const TextStyle(
-        fontSize: 36,
-        fontWeight: FontWeight.w500,
-      );
-
   static TextStyle get biggerTitle => const TextStyle(
         fontSize: 24,
         fontWeight: FontWeight.w500,
